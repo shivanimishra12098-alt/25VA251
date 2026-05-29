f05b8e4063ddc653a72a2fbd2858444d1b031ef1
shivani mishra <shivanimishra12098@gmail.com>
2026-05-29 10:23:10 +0530

    second semister commit

---
+++ file.js
@@ -0,0 +1,22 @@
+alert("welcome,shivani's creation")
+//confirm("press ok for confirmation")
+//prompt("enter your number")
+//variables
+//fullname ="tony stark";
+//console.log(fullname);
+/*const product={
+    name:"Parker ball pen",
+    rating: 4,
+    offer: 5,
+    price:270,
+};
+conasole.log(product);*/
+//asssignment operator
+let a = 5;
+let b = 4;
+a += 3;
+// a -= 2;
+// a **= 3;
+console.log("a -= ",a);
+// console.log("a += ",a);
+// console.log("a **= ", a);
