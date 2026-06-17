7722ba7b8b4a67614e50d8577ce6fe2f6f7cfc1d
shivanimishra12098-alt <shivanimishra12098@gmail.com>
2026-06-17 23:16:06 +0530

    new commit

---
+++ javascrpt2.js
@@ -0,0 +1,18 @@
+const a = 3, b = 2;
+console.log(a > b);
+// same value, same type
+console.log(5 == 5);  // true
+
+// same value, different type
+console.log(2 == "2");  // true
+
+// different values, same type
+console.log("hello" == "Hello");  // false
+// same value, same type
+console.log(2 != 2);  // false
+
+// same value, different type
+console.log(2 != "2");  // false
+
+// different value, same type
+console.log(2 != 3);  // true
