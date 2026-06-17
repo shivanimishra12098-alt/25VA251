9a6e23b5429e98f6e83b19cff0611cb6950b3696
shivanimishra12098-alt <shivanimishra12098@gmail.com>
2026-06-17 23:30:34 +0530

    commit

---
+++ objects.js
@@ -0,0 +1,24 @@
+// student object
+const student = {
+    firstName: "Jack",
+    rollNo: 32 
+};
+
+console.log(student);
+
+// Output: { firstName: 'Jack', rollNo: 32 }
+// create person object
+const person = {
+    name: "John",
+    age: 20
+};
+console.log(person);
+// Output: { name: "John", age: 20 }
+const dog = {
+    name: "Rocky",
+};
+
+// access property
+console.log(dog.name);
+
+// Output: Rocky
